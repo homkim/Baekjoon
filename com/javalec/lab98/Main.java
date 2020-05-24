d55d26ebd53184a7b43a07b5ab2d25ea5a463ab5
homkim <homkim@naver.com>
2020-05-24 14:21:22 +0900

    time machine

---
+++ com/javalec/lab98/Main.java
@@ -0,0 +1,34 @@
+/* 시간여행 */
+package com.javalec.lab98;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			
+			int move = 0;
+			int xMoveCnt = 0;
+			int yMoveCnt = 0;
+			
+			while(true) {
+				move++;
+				
+				x += move;
+				xMoveCnt++;
+				
+				if(x >= y) break; //도착여부
+				
+				y -=  move;
+				yMoveCnt++;
+				if(x >= y) break; //도착여부
+			}
+			System.out.println(xMoveCnt + yMoveCnt);
+		}
+
+	}
+}
