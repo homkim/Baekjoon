8cbc4723da3acfd78d2d5de0eb101d719ca4ce18
homkim <homkim@naver.com>
2020-05-24 10:16:57 +0900

    mid push after 94

---
+++ com/javalec/lab87/Main.java
@@ -0,0 +1,21 @@
+/* 상수의 숫자 읽기  */
+package com.javalec.lab87;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuffer bf = new StringBuffer();
+
+		bf.append(sc.next());		
+		Integer i1 = Integer.parseInt( bf.reverse().toString() );
+		
+		bf.setLength(0);//초기화
+		bf.append(sc.next());
+		Integer i2 = Integer.parseInt( bf.reverse().toString() );
+		
+		System.out.println( Math.max(i1, i2));
+		
+	}
+}
