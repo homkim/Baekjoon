56d142bae46ca21b5bcb2230349bef4ceca9ad5a
homkim <homkim@KORNO005688.samsungds.net>
2020-06-04 18:01:36 +0900

    덩치

--- com/javalec/labc3/Main.java
+++ com/javalec/labc3/Main.java
@@ -1,6 +1,5 @@
 package com.javalec.labc3;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,24 +7,25 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		
-		int[][] iarr = new int[n][3];
+		int[][] arr = new int[n][2];
+		int[] rnk = new int[n];
+		
 		for (int i = 0; i < n; i++) {
-			iarr[i][0] = sc.nextInt();
-			iarr[i][1] = sc.nextInt();
-			iarr[i][2] = i + 1;
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
 		}
 		
-		Arrays.sort(iarr, (o1,o2) -> {
-			if(o1[0] == o2[0]) {
-				return Integer.compare(o1[1], o2[1]);
-			} else {
-				return Integer.compare(o1[0],o2[0]);
+		for(int i=0; i<n;i++) {
+			for(int j=0; j<n;j++) {
+				if(arr[i][0]>arr[j][0] && arr[i][1]>arr[j][1]) {
+					rnk[j]++;
+				}
 			}
-		});
-		
-		for (int i = 0; i < iarr.length; i++) {
-			
-			System.out.println(iarr[i][0] + " " + iarr[i][1] + " " + iarr[i][2]);
 		}
+		
+		for(int i=0; i<n;i++) {		
+			System.out.print( (rnk[i] + 1) + " ");
+		}
+		
 	}
 }
